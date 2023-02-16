26434b88e125ae3fdffcc89776057894988d97c6
Commentatk media <apps@commentatk-media.com>
2023-02-16 10:54:22 +0200

    update NovaPermissions.php hide resource from navigation

--- src/NovaPermissions.php
+++ src/NovaPermissions.php
@@ -1,80 +1,96 @@
 <?php
 
-
 namespace GrapheneICT\NovaPermissions;
 
 use GrapheneICT\NovaPermissions\Nova\Permission;
 use GrapheneICT\NovaPermissions\Nova\Role;
-use Laravel\Nova\Tool;
 use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
 
 class NovaPermissions extends Tool
 {
-	/**
-	 * @var mixed
-	 */
-	public $permissionResource = Permission::class;
-	
-	/**
-	 * @var mixed
-	 */
-	public $roleResource = Role::class;
-	
-	/**
-	 * Perform any tasks that need to happen when the tool is booted.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		Nova::script('NovaPermissions', __DIR__ . '/../dist/js/tool.js');
-		Nova::style('NovaPermissions', __DIR__ . '/../dist/css/tool.css');
-		
-		Nova::resources([
-			$this->roleResource,
-			$this->permissionResource,
-		]);
-	}
-	
-	/**
-	 * Get the displayable name of the resource tool.
-	 *
-	 * @return string
-	 */
-	public function name()
-	{
-		return 'Roles & Permissions';
-	}
-	
-	/**
-	 * @param  string  $permissionResource
-	 * @return mixed
-	 */
-	public function permissionResource(string $permissionResource)
-	{
-		$this->permissionResource = $permissionResource;
-		
-		return $this;
-	}
-	
-	/**
-	 * Build the view that renders the navigation links for the tool.
-	 *
-	 * @return \Illuminate\View\View
-	 */
-	public function renderNavigation()
-	{
-		return view('nova-permissions::navigation');
-	}
-	
-	/**
-	 * @param  string  $roleResource
-	 * @return mixed
-	 */
-	public function roleResource(string $roleResource)
-	{
-		$this->roleResource = $roleResource;
-		
-		return $this;
-	}
+    /**
+     * @var mixed
+     */
+    public $permissionResource = Permission::class;
+
+    /**
+     * @var mixed
+     */
+    public $roleResource = Role::class;
+
+    /**
+     * showInNavigation
+     *
+     * @var bool
+     */
+    public $showInNavigation = true;
+
+    /**
+     * hideInNavigation function
+     *
+     * @return void
+     */
+    public function hideInNavigation()
+    {
+        $this->showInNavigation = false;
+        return $this;
+    }
+
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::script('NovaPermissions', __DIR__ . '/../dist/js/tool.js');
+        Nova::style('NovaPermissions', __DIR__ . '/../dist/css/tool.css');
+
+        Nova::resources([
+            $this->roleResource,
+            $this->permissionResource,
+        ]);
+    }
+
+    /**
+     * Get the displayable name of the resource tool.
+     *
+     * @return string
+     */
+    public function name()
+    {
+        return 'Roles & Permissions';
+    }
+
+    /**
+     * Build the view that renders the navigation links for the tool.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function renderNavigation()
+    {
+        return $this->showInNavigation ? view('nova-permissions::navigation') : "";
+    }
+
+    /**
+     * @param  string  $permissionResource
+     * @return mixed
+     */
+    public function permissionResource(string $permissionResource)
+    {
+        $this->permissionResource = $permissionResource;
+
+        return $this;
+    }
+    /**
+     * @param  string  $roleResource
+     * @return mixed
+     */
+    public function roleResource(string $roleResource)
+    {
+        $this->roleResource = $roleResource;
+
+        return $this;
+    }
 }
